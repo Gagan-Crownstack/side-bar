ea1718be5c5ae13232ae679fce3552e8fb301182
Gagan-Crownstack <gagan@crownstack.com>
2024-09-27 19:04:10 +0530

    sidebar done

---
+++ index.js
@@ -0,0 +1,145 @@
+const data=[
+    {
+        name:'Dashboard',
+        path: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 4.5v15m6-15v15m-10.875 0h15.75c.621 0 1.125-.504 1.125-1.125V5.625c0-.621-.504-1.125-1.125-1.125H4.125C3.504 4.5 3 5.004 3 5.625v12.75c0 .621.504 1.125 1.125 1.125Z" />
+</svg>
+`
+    },
+    {
+        name: 'Swap',
+        path:`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+</svg>
+
+`
+    },
+    {
+        name:'Add Liquidty',
+        path:`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 9.563C9 9.252 9.252 9 9.563 9h4.874c.311 0 .563.252.563.563v4.874c0 .311-.252.563-.563.563H9.564A.562.562 0 0 1 9 14.437V9.564Z" />
+</svg>
+
+`
+    },{
+        name: 'Manage Liquidty',
+        path:`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M4.098 19.902a3.75 3.75 0 0 0 5.304 0l6.401-6.402M6.75 21A3.75 3.75 0 0 1 3 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 0 0 3.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008Z" />
+</svg>
+`
+    },{
+        name:'Thor stake',
+        path:`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
+</svg>
+`
+    },
+    {
+        name: 'Pending Liquidty',
+        path: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
+`
+    },
+    {
+        name:'Vesting',
+        path:`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
+</svg>
+`
+
+    },
+    {
+        name:'Your Wallet',
+        path:`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
+</svg>
+`
+    }, 
+    {
+        name:'Thornode',
+        path:`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+</svg>
+`
+    },
+    {
+        name:'stats',
+        path:`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />
+</svg>
+`
+
+    },
+    {
+        name:'Collapse Sector',
+        path: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+</svg>
+`,
+        func:Collapse
+    }  
+
+]
+
+let isCollapse=false;
+
+document.getElementById('div-box').innerHTML= data.map((dashpoint)=>{ 
+    return (`  <button onclick="${dashpoint.func ? dashpoint.func.name : '() => {}'}()" class=" nav-links cursor-pointer text-white flex justify-center rounded-xl" onmouseover="showOnHover(this)" onmouseout="hideOnHover(this)">
+        <div id='onhov' class='absolute hidden top--8 left-20 rounded-full p-2 bg-[#2e3c56]'> ${dashpoint.name}</div>
+        <span id="dashIcon" class="text-xl flex items-center hover:bg-[#48be85] p-3 rounded-xl">${dashpoint.path}</span>
+        <span id="dash-title" class='flex items-center rounded-xl hover:bg-[#48be85] w-full px-4 p-2'>${dashpoint.name}</span>
+        </button>`)
+}).join('')
+
+
+
+
+function Collapse() {
+    const sidebar = document.getElementById('sideBar');
+    const titles = document.querySelectorAll('#dash-title');
+    const links= document.getElementById('links');
+    const collapselinks= document.getElementById('collapse-links');
+    
+    if (!isCollapse) {
+        sidebar.style.width = "80px";
+        sidebar.style.padding="48px 4px";
+        titles.forEach(title => title.style.display = "none"
+            
+        );
+        links.style.display="none"
+        collapselinks.style.display="block"
+    } else {
+        sidebar.style.width = "288px";
+        sidebar.style.padding="48px 24px";
+        titles.forEach(title => title.style.display = "flex");
+        links.style.display="flex"
+        collapselinks.style.display="none"
+        
+    }
+    isCollapse = !isCollapse;
+    
+}
+
+// const onhovdisplay= document.querySelectorAll('#onhov');
+
+// isCollapse && onhovdisplay.document.addEventListener("hover",()=>{
+//     onhovdisplay.style.display="flex"; 
+// } );
+
+function showOnHover(button) {
+   if(isCollapse){
+    const tooltip = button.querySelector('div');
+    tooltip.style.display = 'flex';
+   }
+}
+
+function hideOnHover(button) {
+    const tooltip = button.querySelector('div');
+    tooltip.style.display = 'none';
+}
+
+
+
